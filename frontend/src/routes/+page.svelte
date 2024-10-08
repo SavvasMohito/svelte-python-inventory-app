6746fe668823a75e5e86b11884f8a472cb8a4fd4
Savvas Mantzouranidis <s.mantzouranidis@bristol.ac.uk>
2024-10-08 13:31:50 +0000

    first successful backend connection

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+	const { data } = $props();
+	const { message }: { message: string } = data;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h2 class="mt-10">Backend message:</h2>
+<p>{message}</p>
