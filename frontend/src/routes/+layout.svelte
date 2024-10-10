e2fa076f1dc79b4acb73ac4c4f16b8978fba4056
Savvas Mantzouranidis <s.mantzouranidis@bristol.ac.uk>
2024-10-10 19:32:50 +0000

    add footer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,6 +4,14 @@
 	let { children } = $props();
 </script>
 
-<div class="p-10">
+<div class="flex h-screen flex-col p-10">
 	{@render children()}
+	<div class="absolute bottom-2 self-center text-sm text-gray-500">
+		Developed by <a
+			class="text-primary hover:underline"
+			href="https://github.com/SavvasMohito"
+			target="_blank">Savvas Mantzouranidis</a
+		>
+		for the first assignment of PA2577
+	</div>
 </div>
